6d5639fa97b1a4583327139341e747fd723e108e
Graham Bleaney <gbleaney@gmail.com>
2023-06-04 20:10:48 +0000

    Add implementation and tests for a weather data aggregator fu nction

--- interview/weather.py
+++ interview/weather.py
@@ -1,3 +1,109 @@
+from io import TextIOBase
+import csv
+from datetime import datetime, date
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import List, Dict
+from decimal import Decimal
 
-def process_csv(reader, writer):
-    writer.write(f"Saw {len(reader.readlines())} lines" + "\n")
+
+@dataclass
+class Reading:
+    datetime: datetime
+    temperature: float
+
+
+def process_csv(reader: TextIOBase, writer: TextIOBase):
+    """Read in CSV-formatted weather data from the specified 'reader'
+    and output a CSV-formatted aggregation to the specified 'writer'.
+
+    The CSV inputted through 'reader' must contain at least "Station Name",
+    "Air Temperature", and "Measurement Timestamp" columns, in any order.
+
+    The CSV outputted through 'writer' will contain "Station Name", "Date",
+    "Min Temp", "Max Temp", "First Temp", and "Last Temp" columns
+    """
+
+    # Note: in the writing of this function, I made the following assumptions:
+    # 1. CSV columns are named and formatted exactly as shown in test data (ie. I'm assuming no malformed inputs)
+    # 2. All data for a given day appear in consecutive rows within the CSV, without data from other days interspersed (see `test_data_ordering` for validation of this in the test data).
+    # 3. Outputting data with a CRLF ("\r\n" in python), rather than the more standard Linux/OSX LF only ("\n" in Python), is acceptable in this case given that the CSV spec specifies CRLF explicity: https://datatracker.ietf.org/doc/html/rfc4180
+
+    field_names = ["Station Name", "Date", "Min Temp",
+                   "Max Temp", "First Temp", "Last Temp"]
+    csv_writer = csv.writer(writer)
+    csv_writer.writerow(field_names)
+
+    previous_timestamp = datetime(1, 1, 1)
+    daily_data = defaultdict(list)
+
+    # Note: csv library iterates using the `__next__` method and thus should
+    # be streaming in from `reader`, and not loading everything into memory
+    # (other than any internal buffering that may occur)
+    for line in csv.DictReader(reader):
+        current_timestamp = datetime.strptime(
+            line["Measurement Timestamp"], "%m/%d/%Y %I:%M:%S %p")
+        if current_timestamp.date() != previous_timestamp.date():
+            # We've rolled over to a new day's data, so let's flush the data
+            # from the day we were previously processing and start a new day.
+            output_day(daily_data, previous_timestamp.date(), csv_writer)
+            daily_data = defaultdict(list)
+
+        # Note: I use decimal here to prevent issues from round tripping
+        # str -> float -> str for values that can not be represented
+        # exactly as floats. While Python will usually output the right
+        # value even for pathological inputs like 1.1 (ctrl + f "David Gay"
+        # on this page for why: https://docs.python.org/3/whatsnew/3.1.html#other-language-changes),
+        # I still use Decimal here to avoid relying on that platform-dependent
+        # functionality and unintuative results like an input of
+        # "1.100000000000000088817841970012523233890533447265625" being outputted
+        # as "1.1".
+        current_temperature = Decimal(line["Air Temperature"])
+        current_station = line["Station Name"]
+        daily_data[current_station].append(Reading(
+            current_timestamp, current_temperature))
+        previous_timestamp = current_timestamp
+
+    # Since the loop above flushes `daily_data` when it sees a new day start,
+    # we need to do one final flush here for the final day's data.
+    output_day(daily_data, previous_timestamp.date(), csv_writer)
+
+    writer.flush()
+
+
+def output_day(daily_data: Dict[str, List[Reading]], current_date: date, csv_writer: csv.writer) -> List[str]:
+    """Output the specified 'daily_data' for the specified 'current_data' to
+    the specified 'csv_writer'. 'daily_data' must be a dict maping station
+    names to a list of readings all taken on 'current_date'. 'csv_writer' must
+    already be initialized with at least the header row already written.
+    """
+    formatted_date = current_date.strftime("%m/%d/%Y")
+    for station, readings in daily_data.items():
+        readings: List[Reading]
+        # Note: Technically this iterates `readings` four times, when we could
+        # find these values by iterating once with some manual tracking.
+        # Given that the time complexity of both solutions (O(4N) = O(N)) is the
+        # same, I've opted for the cleaner solution here.
+        min_temperature = format_decimal(min(
+            readings, key=lambda reading: reading.temperature).temperature)
+        max_temperature = format_decimal(max(
+            readings, key=lambda reading: reading.temperature).temperature)
+        first_temperature = format_decimal(min(
+            readings, key=lambda reading: reading.datetime).temperature)
+        last_temperature = format_decimal(max(
+            readings, key=lambda reading: reading.datetime).temperature)
+
+        csv_writer.writerow([station, formatted_date, min_temperature,
+                            max_temperature, first_temperature, last_temperature])
+
+
+def format_decimal(number: Decimal) -> str:
+    """Take the given 'number' and return a string representation of it
+    with all it's original precision preserved AND at least a trailing .0
+    if no other decimals are present.
+    """
+    # This is a little hack to emulate the behaviour of the float type
+    # where it always displays with a *minimum* precision of 1 decimal.
+    # There is no way to specify minimum precision in a format specifier,
+    # so we do this little hack
+    return max('{:.1f}'.format(number), str(number), key=len)
--- test/weather_test.py
+++ test/weather_test.py
@@ -1,9 +1,164 @@
 from interview import weather
 import io
+import pytest
+from datetime import datetime
+import csv
 
 
-def test_replace_me():
-    reader = io.StringIO("Line One\nLine Two\n")
+README_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Foster Weather Station,01/01/2016 11:00:00 PM,69.0
+Foster Weather Station,01/01/2016 08:00:00 PM,70.0
+Foster Weather Station,01/01/2016 07:00:00 PM,70.0
+Foster Weather Station,01/01/2016 06:00:00 PM,72.0
+Foster Weather Station,01/01/2016 05:00:00 PM,72.0
+Foster Weather Station,01/01/2016 04:00:00 PM,73.0
+Foster Weather Station,01/01/2016 03:00:00 PM,69.0
+Foster Weather Station,01/01/2016 02:00:00 PM,70.0
+Foster Weather Station,01/01/2016 01:00:00 PM,70.0
+Foster Weather Station,01/01/2016 12:00:00 PM,70.0
+Foster Weather Station,01/01/2016 11:00:00 AM,70.0
+Foster Weather Station,01/01/2016 10:00:00 AM,70.0
+Foster Weather Station,01/01/2016 09:00:00 AM,70.0
+Foster Weather Station,01/01/2016 08:00:00 AM,71.0
+Foster Weather Station,01/01/2016 07:00:00 AM,72.0
+Foster Weather Station,01/01/2016 06:00:00 AM,72.0
+Foster Weather Station,01/01/2016 05:00:00 AM,71.0
+Foster Weather Station,01/01/2016 04:00:00 AM,69.0
+Foster Weather Station,01/01/2016 03:00:00 AM,67.0
+Foster Weather Station,01/01/2016 02:00:00 AM,64.0
+Foster Weather Station,01/01/2016 01:00:00 AM,67.0
+Foster Weather Station,01/01/2016 12:00:00 AM,67.0
+"""
+
+README_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Foster Weather Station,01/01/2016,64.0,73.0,67.0,69.0
+"""
+
+EMPTY_STATIONS_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+"""
+
+EMPTY_STATIONS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+"""
+
+SINGLE_STATIONS_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 11:00:00 PM,68.0
+"""
+
+SINGLE_STATIONS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,68.0,68.0,68.0,68.0
+"""
+
+MULTIPLE_STATIONS_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 11:00:00 PM,68.0
+Station B,01/01/2016 08:00:00 PM,70.0
+Station C,01/01/2016 07:00:00 PM,71.0
+"""
+
+MULTIPLE_STATIONS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,68.0,68.0,68.0,68.0
+Station B,01/01/2016,70.0,70.0,70.0,70.0
+Station C,01/01/2016,71.0,71.0,71.0,71.0
+"""
+
+MULTIPLE_DAYS_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 11:00:00 PM,68.0
+Station A,01/02/2016 08:00:00 PM,70.0
+Station A,01/03/2016 07:00:00 PM,71.0
+"""
+
+MULTIPLE_DAYS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,68.0,68.0,68.0,68.0
+Station A,01/02/2016,70.0,70.0,70.0,70.0
+Station A,01/03/2016,71.0,71.0,71.0,71.0
+"""
+
+MULTIPLE_STATIONS_AND_DAYS_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 1:00:00 PM,67.0
+Station A,01/01/2016 2:00:00 PM,68.0
+Station A,01/01/2016 3:00:00 PM,65.0
+Station A,01/01/2016 4:00:00 PM,66.0
+Station B,01/01/2016 1:00:00 PM,63.0
+Station B,01/01/2016 2:00:00 PM,64.0
+Station B,01/01/2016 3:00:00 PM,61.0
+Station B,01/01/2016 4:00:00 PM,62.0
+Station A,01/02/2016 1:00:00 PM,57.0
+Station A,01/02/2016 2:00:00 PM,58.0
+Station A,01/02/2016 3:00:00 PM,55.0
+Station A,01/02/2016 4:00:00 PM,56.0
+Station B,01/02/2016 1:00:00 PM,53.0
+Station B,01/02/2016 2:00:00 PM,54.0
+Station B,01/02/2016 3:00:00 PM,51.0
+Station B,01/02/2016 4:00:00 PM,52.0
+"""
+
+MULTIPLE_STATIONS_AND_DAYS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,65.0,68.0,67.0,66.0
+Station B,01/01/2016,61.0,64.0,63.0,62.0
+Station A,01/02/2016,55.0,58.0,57.0,56.0
+Station B,01/02/2016,51.0,54.0,53.0,52.0
+"""
+
+LONG_DECIMAL_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 11:00:00 PM,68.12345678900987654321
+"""
+
+LONG_DECIMAL_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,68.12345678900987654321,68.12345678900987654321,68.12345678900987654321,68.12345678900987654321
+"""
+
+MISSING_TRAILING_ZERO_INPUT = """Station Name,Measurement Timestamp,Air Temperature
+Station A,01/01/2016 11:00:00 PM,68
+"""
+
+MISSING_TRAILING_ZERO_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+Station A,01/01/2016,68.0,68.0,68.0,68.0
+"""
+
+EXTRA_HEADERS_INPUT = """Station Name,Measurement Timestamp,Air Temperature,Wet Bulb Temperature,Humidity,Rain Intensity,Interval Rain,Total Rain,Precipitation Type,Wind Direction,Wind Speed,Maximum Wind Speed,Barometric Pressure,Solar Radiation,Heading,Battery Life,Measurement Timestamp Label,Measurement ID
+63rd Street Weather Station,12/31/2016 11:00:00 PM,-1.3,-2.8,73,0,0,39.3,0,264,2.2,3.2,992.3,5,354,11.8,12/31/2016 11:00 PM,63rdStreetWeatherStation201612312300
+"""
+
+EXTRA_HEADERS_OUTPUT = """Station Name,Date,Min Temp,Max Temp,First Temp,Last Temp
+63rd Street Weather Station,12/31/2016,-1.3,-1.3,-1.3,-1.3
+"""
+
+# Note: These tests currently depend on output order being stable. With the
+# current implementation, order should always be stable becuase I use dict
+# and list, which are both stable in Python. See this reference on the 
+# stability of dict: https://stackoverflow.com/questions/39980323/are-dictionaries-ordered-in-python-3-6
+
+@pytest.mark.parametrize(
+    "given_input,expected_output",
+    [(README_INPUT, README_OUTPUT),
+     (EMPTY_STATIONS_INPUT, EMPTY_STATIONS_OUTPUT),
+     (SINGLE_STATIONS_INPUT, SINGLE_STATIONS_OUTPUT),
+     (MULTIPLE_STATIONS_INPUT, MULTIPLE_STATIONS_OUTPUT),
+     (MULTIPLE_DAYS_INPUT, MULTIPLE_DAYS_OUTPUT),
+     (MULTIPLE_STATIONS_AND_DAYS_INPUT, MULTIPLE_STATIONS_AND_DAYS_OUTPUT),
+     (LONG_DECIMAL_INPUT, LONG_DECIMAL_OUTPUT),
+     (MISSING_TRAILING_ZERO_INPUT, MISSING_TRAILING_ZERO_OUTPUT)],
+)
+def test_inputs_produce_expected_output(given_input: str, expected_output: str) -> None:
+    reader = io.StringIO(given_input)
     writer = io.StringIO()
     weather.process_csv(reader, writer)
-    assert writer.getvalue() == "Saw 2 lines\n"
+    expected_output = io.StringIO(expected_output)
+
+    writer.seek(0)
+    for line in writer:
+        assert line.rstrip() == expected_output.readline().rstrip()
+
+    # Make sure we've read the entirity of `expected_output`
+    assert not expected_output.readline()
+
+
+def test_data_ordering():
+    # This test exists to validate my assumption that test data is sorted from newest to oldest
+    with open("data/chicago_beach_weather.csv") as weather_file:
+        csv_reader = csv.DictReader(weather_file)
+        previous_timestamp = datetime(9999, 1, 1)
+        for row in csv_reader:
+            timestamp = datetime.strptime(
+                row["Measurement Timestamp"], "%m/%d/%Y %I:%M:%S %p")
+            assert timestamp <= previous_timestamp
+            previous_timestamp = timestamp
